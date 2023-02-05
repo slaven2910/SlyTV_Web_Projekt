ac0c87bac733bd776d6fd17e1202833da0ce34b9
Slaven Mlinaric <slaven2910@gmail.com>
2023-02-05 17:02:12 +0100

    Works

--- components/footer.php
+++ components/footer.php
@@ -4,14 +4,6 @@
   <!-- Bezugnahme auf Design-Elemente von [Bootstrap 5.2.3]. -->
   <div class="container">
     <!-- Bezugnahme auf Design-Elemente von [Bootstrap 5.2.3]. -->
-    <div class="row">
-      <!-- Bezugnahme auf Design-Elemente von [Bootstrap 5.2.3]. -->
-      <div class="col text-center">
-        <img src="assets/images/logo-white.png" alt="Your Logo" style="width: 200px;">
-      </div>
-      <!-- Bezugnahme auf Design-Elemente von [Bootstrap 5.2.3]. -->
-      
-    </div>
     <div class="row text-center justify-content-center ">
         <!-- Bezugnahme auf Design-Elemente von [Bootstrap 5.2.3]. -->
         <div class="col-3"><a class="text-white" href="https://github.com/slaven2910/SlyTV_Web_Projekt/blob/main/Legal_Notice_New.txt">Legal stuff</a> </div>
@@ -23,4 +15,5 @@
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <!-- Zugriff auf die Deklaration des JavaScripts von [Bootstrap 5.2.3]. -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
 </footer>
--- components/navbar.php
+++ components/navbar.php
@@ -81,13 +81,9 @@
         </ul>
       </div>
     </nav>
-
-
-
   </section>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://use.fontawesome.com/22109dfc6e.js"></script>
- 
 </body>
 
 </html>
