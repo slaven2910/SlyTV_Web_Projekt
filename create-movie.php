76ff4b0e36c7019557a719ac0597a33575f4c73d
yannick1327 <yannick.diehl2003@gmail.com>
2023-02-05 17:42:37 +0100

    create movie files

---
+++ create-movie.php
@@ -0,0 +1,66 @@
+<?php
+// Check if user is logged in
+if(!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] != true){
+    // user is not logged in
+    // redirect user to login page
+    header("Location: login.php?message=login_required_to_post_reviews&movie_id=$movie_id");
+    exit;
+}
+
+else{
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add-a-Movie</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+<?php include('./components/navbar.php'); ?>
+<div>
+    
+    <form action="create-movie-verfication.php" method="POST">
+    <h2>Add a movie</h2>
+    <br>
+    <div class="form-outline form-white mb-4">
+        <input type="name" name="title" />
+        <label>Movie Name</label>
+    </div>
+    <br>
+    <div class="form-outline form-white mb-4">
+        <input type="month" name="publishingyear" />
+        <label>Publishing Year</label>
+    </div>
+    <br>
+    <div class="form-outline form-white mb-4">
+        <input type="name" name="genre" />
+        <label>Genre</label>
+    </div>
+    <br>
+    <div class="form-outline form-white mb-4">
+        <input type="name" name="plot" />
+        <label>Plot</label>
+    </div>
+    <br>
+    <div class="form-outline form-white mb-4">
+        <input type="name" name="actors" />
+        <label>Actors</label>
+    </div>
+    <br>
+    <div class="form-outline form-white mb-4">
+        <input type="file" name="image" />
+        <label>Movie Image</label>
+    </div>
+    <input type="submit" value="Submit">
+    </form>
+</div>
+
+</body>
+</html>
+
+<?php } ?>
