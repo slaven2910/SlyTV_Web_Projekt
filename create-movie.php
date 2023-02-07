df0689c1dc22fb6e507ccf2802b9d54c101412de
yannick1327 <yannick.diehl2003@gmail.com>
2023-02-07 22:05:15 +0100

    finished movie upload

--- create-movie.php
+++ create-movie.php
@@ -5,7 +5,7 @@ include 'scripts/connect.php';
 if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] != true) {
     // user is not logged in
     // redirect user to login page
-    header("Location: login.php?message=login_required_to_post_reviews");
+    header("Location: login.php?message=login_required_to_add_a_movie");
     exit;
 } ?>
 
@@ -22,36 +22,88 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] != true) {
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                 <div class="card" style="border-radius: 1rem;">
                     <div class="card-body p-5 text-center ">
-                        <form action="scripts/create-movie-verfication.php" method="POST">
+                        <form action="scripts/create-movie-verification.php" method="POST" enctype="multipart/form-data">
                             <h2 class="textOnCard fw-bold mb-2 text-uppercase">Add a Movie</h2>
                             <p class="textOnCard mb-5">Please enter the needed information!</p>
+                            
+                            <?php if (isset($_GET["error"])) { ?>
+                                <!-- alert from https://getbootstrap.com/docs/5.2/components/alerts/ -->
+                                <div class="alert alert-danger" role="alert">
+                                    <?php echo $_GET["error"]; ?>
+                                </div>
+                            <?php } ?>
+
+                            <?php if (isset($_GET["title"])) { ?>
                             <div class="form-outline form-white mb-4">
+                                <input type="name" name="title" value="<?php echo $_GET["title"]; ?>"/>
+                                <label>Movie Name</label>
+                            </div>
+                            <?php } else { ?>
+                                <div class="form-outline form-white mb-4">
                                 <input type="name" name="title" />
                                 <label>Movie Name</label>
                             </div>
+                            <?php } ?>
+
+                            <?php if (isset($_GET["publishingyear"])) { ?>
+                            <br>
+                            <div class="form-outline form-white mb-4">
+                                <input type="month" name="publishingyear" value="<?php echo $_GET["publishingyear"]; ?>"/>
+                                <label>Publishing Year</label>
+                            </div>
+                            <?php } else { ?>
                             <br>
                             <div class="form-outline form-white mb-4">
                                 <input type="month" name="publishingyear" />
                                 <label>Publishing Year</label>
                             </div>
+                            <?php } ?>
+
+                            <?php if (isset($_GET["genre"])) { ?>
+                            <br>
+                            <div class="form-outline form-white mb-4">
+                                <input type="name" name="genre" value="<?php echo $_GET["genre"]; ?>"/>
+                                <label>Genre</label>
+                            </div>
+                            <?php } else { ?>
                             <br>
                             <div class="form-outline form-white mb-4">
                                 <input type="name" name="genre" />
                                 <label>Genre</label>
                             </div>
+                            <?php } ?>
+
+                            <?php if (isset($_GET["plot"])) { ?>
+                            <br>
+                            <div class="form-outline form-white mb-4">
+                                <input type="name" name="plot" value="<?php echo $_GET["plot"]; ?>"/>
+                                <label>Plot</label>
+                            </div>
+                            <?php } else { ?>
                             <br>
                             <div class="form-outline form-white mb-4">
                                 <input type="name" name="plot" />
                                 <label>Plot</label>
                             </div>
+                            <?php } ?>
+
+                            <?php if (isset($_GET["actors"])) { ?>
+                            <br>
+                            <div class="form-outline form-white mb-4">
+                                <input type="name" name="actors" value="<?php echo $_GET["actors"]; ?>"/>
+                                <label>Actors</label>
+                            </div>
+                            <?php } else { ?>
                             <br>
                             <div class="form-outline form-white mb-4">
                                 <input type="name" name="actors" />
                                 <label>Actors</label>
                             </div>
+                            <?php } ?>
+
                             <br>
                             <div class="form-outline form-white mb-4">
-                                <input type="file" name="image" />
+                                <input type="file" name="image" accept="image/png, image/jpeg, image/jpg"/>
                                 <label>Movie Image</label>
                             </div>
                             <input type="submit" value="Submit">
--- scripts/create-movie-verification.php
+++ scripts/create-movie-verification.php
@@ -1,5 +1,5 @@
 <?php
-include 'connect.php';
+include '../scripts/connect.php';
 
 if (!$dbConn) {
     echo "Ein Fehler ist aufgetreten.\n";
@@ -11,17 +11,50 @@ $publishingyear = $_POST["publishingyear"];
 $genre = $_POST["genre"];
 $plot = $_POST["plot"];
 $actors = $_POST["actors"];
-// weiß nicht was da übergeben wird
-$image = $_POST["image"];
+
+//save useradata to give it back if image is not the right format
+$userdata = "title=" . $title . "&publishingyear=" . $publishingyear . "&genre=" . $genre . "&plot=" . $plot . "&actors=" . $actors;
+
+//get filetype of the uploaded image
+$filetype = $_FILES['image']['type'];
+
+//validates if the image format is ok
+if(
+    $filetype != "image/jpg" &&
+	$filetype != "image/jpeg" &&
+    $filetype != "image/png" 
+){
+    echo "Sorry this file type is not supported";
+    header("Location: ../create-movie.php?error=file_format_is_not_supported&$userdata");
+    exit;
+}
+
+//get more values from the uploaded image
+$filename = $_FILES['image']['name'];
+$tmpfilename = $_FILES['image']['tmp_name'];
+
+//rename the uploaded file and move it to the images folder
+$fileasarry = explode('.', $filename);
+$fileextesion = end($fileasarry);
+$newfilename = str_replace(" ", "-", $title) . "." . $fileextesion;
+$destination = "../assets/images/". $newfilename;
+move_uploaded_file($tmpfilename, $destination);
+
+//count the rows in Movies to determine id
+$query = "SELECT * FROM public.\"Movies\"";
+$rowtable = pg_query($dbConn, $query);
+$id = 1 + pg_num_rows($rowtable);
 
 
-// Hier kommt die restliche Logik hin
-$query = "";
+// SQL statement to add movie to the database
+$statement = "INSERT INTO public.\"Movies\" (id, title, genre, publishingyear, plot, image, actors) VALUES ($id, '$title', '$genre', $publishingyear, '$plot', '$newfilename', '$actors')";
+$queryResult = pg_query($dbConn, $statement);
 
-
-
-// ganz am Ende, wenn alles funktioniert das ausführen:
-// header("Location: ../index.php");
-// exit();
-
+if($queryResult){
+    header("Location: ../movie-reviews.php?movie_id=$id");
+    exit();
+} else  {
+    header("Location: ../create-movie.php?error=php-insert-failed&$userdata");
+    exit();
+}
 ?>
